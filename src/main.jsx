e11e146707af6ab53b7e29af9d963fde17c1740e
Rahul Kumar Sharma <rahulkumarsharma7898@gmail.com>
2025-09-29 14:40:32 +0530

    done again

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Loading from './components/loading.jsx'
+// import Loading from './components/loading.jsx'
 
 
 createRoot(document.getElementById('root')).render(
